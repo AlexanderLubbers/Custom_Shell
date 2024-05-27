92cc70a9bb737dd49507171707d4f50964cf2a39
AlexanderLubbers <alexanderb.lubbers@gmail.com>
2024-05-27 19:31:11 -0400

    added functionality to the directory display function and cd command

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <direct.h>
 #define TOKEN_DELIMETER " \t\r\n\a\""
 // important: the size of a pinter type depends on system architecture
 // ex: a pointer is 8 bytes on a 64 bit system
@@ -17,6 +18,17 @@ int status = 1;
 int shell_cd(char **args)
 {
     printf("cd command called\n");
+    if (args[1] == NULL)
+    {
+        return 1;
+    }
+    int result = chdir(args[1]);
+    if (result == -1)
+    {
+        printf("Shell: invalid parameters");
+    } else {
+        return 1;
+    }
 }
 int shell_help(char **args)
 {
@@ -26,16 +38,41 @@ int shell_exit(char **args)
 {
     exit(EXIT_SUCCESS);
 }
+int shell_display_directory(char **args)
+{
+    char *buffer;
+    // getcwd gets the full path of the current working directory and then stores it in the buffere variable
+    // the first input for getcwd is the storage location for the path which can be left NULL
+    // second input specifies the maximum length for the path. an error will be reported if the maxlength is exceeded
+    //_getcwd returns a pointer to the buffer which containes a string that represents the path of the current
+    // working directory
+    // by passing in NULL and 0 into the function, this tells the function to dynamically allocate the buffer needed
+    // to store the current working directory. This means that the corrent amount of memory will automoatically be allocated
+    if ((buffer = _getcwd(NULL, 0)) == NULL)
+    {
+        perror("Shell: directory command error");
+    }
+    else
+    {
+        printf("%s\n", buffer);
+    }
+
+    // the buffer variable must be freed from memory to prevent memory leaks
+    free(buffer);
+    return 1;
+}
 
 // list of builtin shell commands and then their coresponding functions
 char *builtin_string[] = {
     "cd",
     "help",
-    "exit"};
+    "exit",
+    "directory"};
 int (*builtin_functions[])(char **) = {
     &shell_cd,
     &shell_help,
-    &shell_exit};
+    &shell_exit,
+    &shell_display_directory};
 
 // fp (file pointer) points to a FILE object that gets the stream that the operation will be used on
 char *read_line(FILE *fp, size_t size)
